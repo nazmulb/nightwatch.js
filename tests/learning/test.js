e7b9c17ba3a0d19954ca1b4bd8e046ca8f717967
Nazmul Basher <nazmulbasher@Nazmuls-MacBook-Air.local>
2017-07-12 13:43:50 +0600

    chenged some contents

--- tests/learning/test.js
+++ tests/learning/test.js
@@ -5,12 +5,12 @@ module.exports = {
       .windowMaximize()
       .url(browser.launchUrl)
       .waitForElementVisible('body', 1000)
-      .setValue('[name="username"]','buyer.admin')
-      .setValue('[name="password"]','buyer.admin')
+      .setValue('[name="username"]', 'buyer.admin')
+      .setValue('[name="password"]', 'buyer.admin')
       .verify.value( '[name="btnSumbit"]', 'Log in' )
       .click('[name="btnSumbit"]')
       .pause(1000)
-      .assert.containsText('.logo','Field Nation')
+      .assert.containsText('.logo', 'Field Nation')
       .pause(1000)
       .end();
   }          
