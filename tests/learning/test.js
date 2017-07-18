9e9ab8d89f12e983a22a9dbf85d14a98e5b01e5f
Nazmul Basher <nazmulbasher@Nazmuls-MacBook-Air.local>
2017-07-18 13:49:49 +0600

    add some logic

--- tests/learning/test.js
+++ tests/learning/test.js
@@ -1,17 +1,46 @@
 module.exports = {
+  '@tags': ['wolc'],
+
+  'before': function (browser){
+    //do nothing.
+  },
+
   'FN LogIn Test' : function (browser) {
-  
-  browser
-      .windowMaximize()
-      .url(browser.launchUrl)
-      .waitForElementVisible('body', 1000)
-      .setValue('[name="username"]', 'buyer.admin')
-      .setValue('[name="password"]', 'buyer.admin')
-      .verify.value( '[name="btnSumbit"]', 'Log in' )
-      .click('[name="btnSumbit"]')
-      .pause(1000)
-      .assert.containsText('.logo', 'Field Nation')
-      .pause(1000)
-      .end();
-  }          
+    browser
+        .windowMaximize()
+        .url(browser.launchUrl)
+        .waitForElementVisible('body', 1000)
+        .setValue('[name="username"]', 'buyer.admin')
+        .setValue('[name="password"]', 'buyer.admin')
+        .verify.value( '[name="btnSumbit"]', 'Log in' )
+        .click('[name="btnSumbit"]')
+        .pause(1000)
+        .assert.containsText('.logo', 'Field Nation')
+        .pause(1000);
+
+        
+  },
+
+  'Click the wo create button': function (browser) {
+      browser
+        .click('body > div.top-bar > div.new-wo-button > a')
+        .waitForElementVisible('#submit-preselect-form', 2000);
+  },
+
+  'Create WO': function (browser) {
+      browser
+        .click('#submit-preselect-form')
+        .pause(1000)
+        .setValue('#title', 'Test WO')
+        .execute('scrollTo(0,0)')
+        .pause(500)
+        .click('body > div.content > div.content-inner.newnav-content.content-inner-new > div.workorder > div.row.action-bar-row > div.action-bar-wrap.ng-scope.normal > div.action-bar > div > div:nth-child(2) > a.btn.btn-default.noncta.mrl');
+  },
+
+    
+  'after': function(browser) {
+    browser
+        .pause(10000)
+        //.end();
+  }
 };
