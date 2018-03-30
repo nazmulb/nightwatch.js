0b6101cebb269f14894553ab84f2cb61da840eec
Nazmul Basher <nazmulbasher@Nazmuls-MacBook-Air.local>
2018-03-30 11:25:04 +0600

    Added some contents

--- custom-commands/commands/login.js
+++ custom-commands/commands/login.js
@@ -2,13 +2,13 @@ exports.command = function () {
     this
         .windowMaximize()
         .url(this.launchUrl)
-        .waitForElementVisible('body', 1000)
-        .setValue('[name="username"]', 'buyer.admin')
-        .setValue('[name="password"]', 'buyer.admin')
-        .verify.value( '[name="btnSumbit"]', 'Log in' )
-        .click('[name="btnSumbit"]')
+        .waitForElementVisible('body', 3000)
+        .setValue('#email', 'buyer.admin')
+        .setValue('#password', 'buyer.admin')
         .pause(1000)
-        .assert.containsText('.logo', 'Field Nation')
+        .click('#Login > div > div > div > div > form > div:nth-child(5) > div > div.Median__row__item__3UTco.Median__row__item--omega__2CyQy > div > span > button > span')
+        .pause(3000)
+        //.assert.containsText('.logo', 'Field Nation')
         .pause(1000);
 
     return this;
